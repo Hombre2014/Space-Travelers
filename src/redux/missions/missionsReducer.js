8c00ada96e0a86c0f5901754574f848ad416b811
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-01-31 11:58:11 +0100

    Setup store and related file structures

---
+++ src/redux/missions/missionsReducer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function missionsReducer() {
+  return <div></div>;
+}
+
+export default missionsReducer;
---
+++ src/redux/rockets/rocketsReducer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function rocketsReducer() {
+  return <div></div>;
+}
+
+export default rocketsReducer;
