c13b32e3f9e746dafe40a2d3dc852f46a217bf29
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-01-31 20:36:23 +0100

    Fix linter errors

--- src/redux/missions/missionsFetch.js
+++ src/redux/missions/missionsFetch.js
@@ -7,19 +7,6 @@ const missionsFetch = () => async (dispatch) => {
   const res = await fetch(URL);
   const missions = await res.json();
 
-  // console.log(data);
-  // const missions = res.data();
-  console.log(missions);
-
-  // const missions = Object.entries(data).map(([mission_id, missionInfo]) => {
-  //   const { mission_name, description } = missionInfo[0];
-  //   console.log(missionInfo[0]);
-  //   return {
-  //     mission_id,
-  //     mission_name,
-  //     description,
-  //   };
-  // });
   dispatch(getMissions(missions));
 };
 
