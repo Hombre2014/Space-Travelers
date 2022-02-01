848c71c973d6801601b80fd32806cfb2c77cfe56
charlesgobina <charles.gobina@gmail.com>
2022-02-01 13:32:36 +0100

    Fetch data from Api

---
+++ src/components/rockets/ReserveButton.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './ReserveButton.css';
+
+const ReservedButton = () => (
+  <button className="rButton" type="button">Reserve Rocket</button>
+);
+
+export default ReservedButton;
--- src/components/rockets/Rockets.js
+++ src/components/rockets/Rockets.js
@@ -1,19 +1,29 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
 import getRockets from '../../redux/rockets/rocketThunk';
+import Rocket from './Rocket';
 
 function Rockets() {
   const availabeRockets = useSelector((state) => state.rocketsReducer);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getRockets());
-  },[dispatch]);
+  }, [dispatch]);
 
   console.log(availabeRockets);
 
   return (
     <div>
-      { availabeRockets.map((rocket) => <p key={rocket.id}>{rocket.rocket_name}</p>)}
+      { availabeRockets.map((rocket) => (
+        <Rocket
+          key={uuidv4()}
+          id={rocket.id}
+          name={rocket.rocket_name}
+          description={rocket.description}
+          images={rocket.flickr_images}
+        />
+      ))}
     </div>
   );
 }
