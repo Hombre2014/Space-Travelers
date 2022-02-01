8e9802bdf813532aaca6e9e0b1649f00f54d9bb9
charlesgobina <charles.gobina@gmail.com>
2022-02-01 08:49:55 +0100

    Display rocket data

--- src/components/rockets/Rockets.js
+++ src/components/rockets/Rockets.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import getRockets from '../../redux/rockets/rocketThunk';
 
 function Rockets() {
-  return <div />;
+  const availabeRockets = useSelector((state) => state.rocketsReducer);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getRockets());
+  },[dispatch]);
+
+  console.log(availabeRockets);
+
+  return (
+    <div>
+      { availabeRockets.map((rocket) => <p key={rocket.id}>{rocket.rocket_name}</p>)}
+    </div>
+  );
 }
 
 export default Rockets;
