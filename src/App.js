390ebe6c8613d22f9d054b215203593581c16205
charlesgobina <charles.gobina@gmail.com>
2022-02-02 17:42:42 +0100

    Implement rocket list on profile page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Menu from './components/Menu/Menu';
 import Rockets from './components/rockets/Rockets';
+import getRockets from './redux/rockets/rocketThunk';
 import Missions from './components/missions/Missions';
 import Profile from './components/profile/Profile';
+import './App.css';
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getRockets());
+  }, [dispatch]);
+
   return (
     <Router>
       <Menu />
@@ -17,6 +25,6 @@ function App() {
     </Router>
 
   );
-}
+};
 
 export default App;
--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
 
-function MyProfile() {
-  return <div />;
-}
+const Profile = () => {
+  const availabeRockets = useSelector((state) => state.rocketsReducer);
 
-export default MyProfile;
+  console.log('profile', availabeRockets);
+  return (
+    <div>
+      <section>
+        <h2>My Missions</h2>
+
+      </section>
+      <section>
+        <h2>My Rockets</h2>
+        <ul>
+          {availabeRockets
+            && availabeRockets
+              .filter((rocket) => rocket.reserved === true)
+              .map((reserved) => (
+                <li key={uuidv4()}>
+                  {reserved.name}
+                </li>
+              ))}
+        </ul>
+      </section>
+    </div>
+  );
+};
+
+export default Profile;
